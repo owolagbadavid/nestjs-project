d83b4912ae4d420cfe946768542b002456a05ba7
DAVID <owolagbadavid@gmail.com>
2023-06-25 09:25:47 +0100

    seed done with transaction

--- src/app.service.ts
+++ src/app.service.ts
@@ -11,7 +11,7 @@ import {
   SupportingDocs,
 } from './entities';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import * as fs from 'fs/promises';
 
 @Injectable()
@@ -32,6 +32,7 @@ export class AppService {
     @InjectRepository(SupportingDocs)
     private supportingDocsRepo: Repository<SupportingDocs>,
     @InjectRepository(Approvals) private approvalsRepo: Repository<Approvals>,
+    private dataSource: DataSource,
   ) {}
   getHello(): string {
     return 'Hello World!';
@@ -53,58 +54,77 @@ export class AppService {
       return 'already seeded';
     }
 
+    // read mock data from json files
     let department: any = await fs.readFile(
       __dirname + '/mock-data/department.json',
       'utf-8',
     );
-
-    department = JSON.parse(department);
-
-    (department = this.departmentRepository.create(department)),
-      await this.departmentRepository.save(department);
-
-    let unit: any = await fs.readFile(
-      __dirname + '/mock-data/unit.json',
-      'utf-8',
-    );
-
-    unit = JSON.parse(unit);
-    unit = this.unitRepository.create(unit);
-    await this.unitRepository.save(unit);
 
     let user: any = await fs.readFile(
       __dirname + '/mock-data/user.json',
       'utf-8',
     );
 
+    let unit: any = await fs.readFile(
+      __dirname + '/mock-data/unit.json',
+      'utf-8',
+    );
+
+    // parse the json data
+    department = JSON.parse(department);
+
+    unit = JSON.parse(unit);
+
     user = JSON.parse(user);
 
+    // create entities from the parsed data
+    department = this.departmentRepository.create(department);
+
+    unit = this.unitRepository.create(unit);
+
     user = this.userRepository.create(user);
-    user = user.map((item) => {
-      item.verified = new Date(item.verified);
-      return item;
-    });
-    await this.userRepository.save(user);
-    unit[0].head = user[1];
-    unit[1].head = user[1 + 8];
-    department[0].head = user[2];
-    department[1].head = user[2 + 8];
 
-    user[0].supervisor = user[1];
-    user[1].supervisor = user[2];
-    user[2].supervisor = user[3];
-    user[3].supervisor = user[4];
-    user[4].supervisor = user[5];
-    user[0 + 8].supervisor = user[1 + 8];
-    user[1 + 8].supervisor = user[2 + 8];
-    user[user.length - 1].supervisor = user[user.length - 2];
-    user[user.length - 2].supervisor = user[4];
+    // initialize the database connection
+    // await this.dataSource.initialize();
 
-    await this.departmentRepository.save(department);
-    await this.unitRepository.save(unit);
-    await this.userRepository.save(user);
+    const queryRunner = this.dataSource.createQueryRunner();
 
-    let lastUser = JSON.parse(`{
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      await queryRunner.manager.save(department);
+
+      await queryRunner.manager.save(unit);
+
+      console.log('department and unit saved');
+      user = user.map((item) => {
+        item.verified = new Date(item.verified);
+        return item;
+      });
+      await queryRunner.manager.save(user);
+      console.log('users saved');
+      unit[0].head = user[1];
+      unit[1].head = user[1 + 8];
+      department[0].head = user[2];
+      department[1].head = user[2 + 8];
+
+      user[0].supervisor = user[1];
+      user[1].supervisor = user[2];
+      user[2].supervisor = user[3];
+      user[3].supervisor = user[4];
+      user[4].supervisor = user[5];
+      user[0 + 8].supervisor = user[1 + 8];
+      user[1 + 8].supervisor = user[2 + 8];
+      user[user.length - 1].supervisor = user[user.length - 2];
+      user[user.length - 2].supervisor = user[4];
+
+      await queryRunner.manager.save(department);
+      await queryRunner.manager.save(unit);
+      await queryRunner.manager.save(user);
+
+      console.log('head and supervisor saved');
+      let lastUser = JSON.parse(`{
       "email": "owolagbadavid@icloud.com",
       "firstName": "Mikey",
       "lastName": "Donnno",
@@ -116,10 +136,18 @@ export class AppService {
       "password": "secret",
       "role": 1
     }`);
-    lastUser.verified = new Date(lastUser.verified);
-    lastUser = this.userRepository.create(lastUser);
-    await this.userRepository.save(lastUser);
+      lastUser.verified = new Date(lastUser.verified);
+      lastUser = this.userRepository.create(lastUser);
+      await queryRunner.manager.save(lastUser);
+      console.log('last user saved, commiting transaction');
 
-    return 'seeded';
+      await queryRunner.commitTransaction();
+      return 'seeded';
+    } catch (error) {
+      queryRunner.rollbackTransaction();
+      return 'seeding error';
+    } finally {
+      queryRunner.release();
+    }
   }
 }
